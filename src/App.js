0d17a919c5546e1524b31d835e6670a52d414b74
khadeom <khadeom10@gmail.com>
2025-11-03 18:39:56 +0530

    task 1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Login, { Demo } from './day_one/Login';
 import ClassDemo from './day_one/ClassDemo';
 import ComponentDemo from './day_one/ComponentDemo';
 
+
+
 function App() {
+  var con = {id:23, sal:3500, name:'om'}
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <Login location='India'></Login>
       <ClassDemo></ClassDemo>
       <Demo></Demo>
-      <ComponentDemo></ComponentDemo>
+      <ComponentDemo emp={con}></ComponentDemo>
     </div>
   );
 }
--- src/day_one/ComponentDemo.jsx
+++ src/day_one/ComponentDemo.jsx
@@ -1,16 +1,38 @@
 import { useState } from "react"
 
-const ComponentDemo = () => {
+const ComponentDemo = (props) => {
 
     const [count,setCount]=useState(0)
+    const [emp,setEmp]=useState({
+      id:props.emp.id,
+      name:props.emp.name,
+      sal:props.emp.sal
+    })
+
+
   return (
 
     
     <div class='demo-class'>
       <h1>Count: {count}</h1>  
-          
+
       <button onClick={()=>setCount(count+1)}> Increment</button>
       <button onClick={()=>setCount(count-1)}> Decrement</button>
+
+      <h1>Employee Details</h1>
+      <div class='list-class'>
+        <ul >
+          <li>Id: {emp.id}</li>
+          <li>Name: {emp.name}</li>
+          <li>Salary: {emp.sal}</li>
+        </ul>
+
+        <button onClick={()=>{setEmp({...emp, sal:emp.sal+5})}}> Increment 5</button>
+        <button onClick={()=>{setEmp({...emp, sal:emp.sal+10})}}> Increment 10</button>
+        <button onClick={()=>{setEmp({...emp, sal:emp.sal-5})}}> Decrement 5</button>
+        <button onClick={()=>{setEmp({...emp, sal:emp.sal-10})}}> Decrement 10</button>
+        <button onClick={()=>{setEmp({...emp, sal:emp.sal-20})}}> Decrement 20</button>
+      </div>
     </div>
 
   )
